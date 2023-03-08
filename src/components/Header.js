2cfe0306b7d1f1330bb72a4ca770179310c1b724
Jordan <jordansampson23@gmail.com>
2023-03-08 10:24:11 -0600

    Added Header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { divide } from "lodash";
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerWrapper">
+                <h1>CV Builder</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
