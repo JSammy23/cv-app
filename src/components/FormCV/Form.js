c5c61613525a44267213056b7f4aeebdf18985ac
Jordan <jordansampson23@gmail.com>
2023-03-08 14:24:51 -0600

    Initialize component

---
+++ src/components/FormCV/Form.js
@@ -0,0 +1,15 @@
+import React, { Component }  from "react";
+import Personal from "./Personal";
+
+
+export class Form extends Component {
+  render() {
+    return (
+      <>
+        <Personal />
+      </>
+    )
+  }
+}
+
+export default Form
---
+++ src/components/FormCV/Personal.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+export class Personal extends Component {
+    constructor(props) {
+        super();
+    }
+  render() {
+    return (
+      <div className='formSection'>
+        <input 
+        type="text"
+        required
+        placeholder='First Name'
+        name='firstName' />
+        <input type="text" 
+        required
+        placeholder='Last Name'/>
+        <input 
+        type="text"
+        required
+        placeholder='Title'
+        name='title' />
+        <label htmlFor="photo">
+          User Image
+          <input
+          type="file"
+          name='photo' />
+        </label>
+        <input
+        type="text"
+        name='address'
+        placeholder='Address' />
+        <input 
+        type="text"
+        name='phoneNumber'
+        placeholder='Phone Number' />
+        <input type="email"
+        name='email'
+        placeholder='Email' />
+        <textarea name="descriptiom" 
+        cols="10" 
+        rows="7"
+        placeholder='Description'></textarea>
+      </div>
+    )
+  }
+}
+
+export default Personal
