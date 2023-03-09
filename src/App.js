7a4620a024909c91f0da07527608cb59ecc7cab0
Jordan <jordansampson23@gmail.com>
2023-03-09 09:06:23 -0600

    Import Form.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
+import Form from './components/FormCV/Form'
 
 export class App extends Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <Form />
+
+      </>
     )
   }
 }
