f3939568ca91f9d9b76161e3999638816486a0c7
Jordan <jordansampson23@gmail.com>
2023-03-09 14:20:00 -0600

    Added cvWrapper, left & right cols.

--- src/components/CVCard.js
+++ src/components/CVCard.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
 
 export class CVCard extends Component {
-    constructor(props) {
-        super(props);
-    }
-
+    
   render() {
 
     const { firstName, lastName, title, address, phoneNumber, email, description } = this.props.personalInfo;
@@ -14,6 +11,23 @@ export class CVCard extends Component {
         <div className='cvHeader'>
             <h1>{firstName} {lastName}</h1>
             <h2>{title}</h2>
+        </div>
+        <div className='cvWrapper'>
+            <div className='cvLeftCol'>
+                <div className='infoSection'>
+                    <h3 className='infoTitle'>Description</h3>
+                    <p>{description}</p>
+                </div>
+                <div className='infoSection'>
+                    <h3 className='infoTitle'>Experience</h3>
+                    {/* <p>{Experience}</p> */}
+                </div>
+            </div>
+            <div className='cvRightCol'>
+                <h3 className='infoTitle'>Personal Details</h3>
+                <h3>Address</h3>
+                <p>{address}</p>
+            </div>
         </div>
       </div>
     )
